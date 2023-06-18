44e526203c78c8a66a86b056d7ce8dfc56e7135b
sey323 <sey323_nm@yahoo.co.jp>
2023-06-18 18:21:55 +0900

    :memo: バッジの名称修正

--- src/components/dataset/DatasetRegister.vue
+++ src/components/dataset/DatasetRegister.vue
@@ -44,7 +44,7 @@
       <div class="validation-result-area">
         <div class="p-3">
           <h2>
-            AIによる投稿可否
+            AIによる検証
             <span
               class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300"
               v-if="fileIsUploaded"
@@ -53,17 +53,17 @@
             <span
               class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300"
               v-else-if="isForbiddenFromAiJudgment"
-              >禁止</span
+              >注意</span
             >
             <span
               class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300"
               v-else-if="isAlertFromAiJudgment"
-              >注意</span
+              >確認の必要あり</span
             >
             <span
               class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300"
               v-else
-              >可能</span
+              >問題なし</span
             >
           </h2>
           <h3>検出されたタグ</h3>
